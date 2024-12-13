26849e0229947d286ef372964c66b2c8ee242a4c
Josh Jones <thejoshjones1@gmail.com>
2024-12-12 17:56:07 -0800

    Goodbye GQL

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import apiRouter from './routes';
-import { buildSchema } from 'graphql';
-import { createHandler } from 'graphql-http/lib/use/express';
-
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-var root = {
-  hello() {
-    return 'Hello world!';
-  }
-};
 
 export const app = express();
 
 // Middleware
 app.use(cors({ origin: true }));
 app.use(express.json());
-app.use(express.raw({ type: 'application/vnd.custom-type' }));
-app.use(express.text({ type: 'text/html' }));
 
 // Healthcheck
 app.get('/', (req, res) => {
-  res.status(200).send({ status: 'ok' });
+  res.status(200).json({ status: 'ok' });
 });
 
-app.all(
-  '/graphql',
-  createHandler({
-    schema: schema,
-    rootValue: root
-  })
-);
-
-// v1 routes
+// REST API
 app.use('/api', apiRouter);
